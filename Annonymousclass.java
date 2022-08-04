99a24ab3bdb1e19b82b17c0cc7cca702999ec173
Vvy123 <110588637+Vvy123@users.noreply.github.com>
2022-08-04 11:49:09 +0530

    first commit

---
+++ Annonymousclass.java
@@ -0,0 +1,17 @@
+interface Animal
+{
+	void type();
+}
+public class Annonymousclass {
+
+	public static void main(String[] args) 
+	{
+		Animal an=new Animal();
+			public void type() 
+			{
+				System.out.println("Annonymous Animal");
+				}
+			an.type();
+	}
+
+}
